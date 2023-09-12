6078c0db8d5a7b31f2d5f2145da37c65af9f6fd5
spags <jamestspagnola@gmail.com>
2023-09-11 20:17:19 -0700

    Add tests for each version.

---
+++ tests-classic.lua
@@ -0,0 +1,13 @@
+debugstack = debug.traceback
+strmatch = string.match
+
+loadfile("Libs/LibStub/LibStub.lua")()
+loadfile("LibTradeSkillRecipes.lua")()
+loadfile("recipes/expansions.lua")()
+loadfile("recipes/1/items.lua")()
+loadfile("recipes/1/enchantments.lua")()
+
+local LibTradeSkillRecipes = LibStub("LibTradeSkillRecipes")
+
+assert(LibTradeSkillRecipes)
+print("Tests Passed!")
---
+++ tests-tbc.lua
@@ -0,0 +1,13 @@
+debugstack = debug.traceback
+strmatch = string.match
+
+loadfile("Libs/LibStub/LibStub.lua")()
+loadfile("LibTradeSkillRecipes.lua")()
+loadfile("recipes/expansions.lua")()
+loadfile("recipes/2/items.lua")()
+loadfile("recipes/2/enchantments.lua")()
+
+local LibTradeSkillRecipes = LibStub("LibTradeSkillRecipes")
+
+assert(LibTradeSkillRecipes)
+print("Tests Passed!")
